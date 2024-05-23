e7cbe4a7b0df40260207340b3b594a3876d89754
AndrewPeng-tcssd <cindy.zhang920@gmail.com>
2024-05-22 19:28:12 -0700

    added level 2

--- script.js
+++ script.js
@@ -5,102 +5,329 @@ kaboom({
     background: (40, 40, 40)
 })
 
-// define gravity
-setGravity(1950)
+scene("level_1", ()=>{
+    // define gravity
+    setGravity(1950)
 
-// load a default sprite
-loadBean()
+    // load a default sprite
+    loadBean()
 
-const speed = 320
+    const speed = 320
 
-add([
-    rect(width(), 40),
-    pos(0, height()-40),
-    area(),
-    body({isStatic: true}),
-    color(0, 255, 0)
-])
+    add([
+        rect(width(), 40),
+        pos(0, height()-40),
+        area(),
+        body({isStatic: true}),
+        color(0, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(250, height()-100),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(250, height()-100),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(435, height()-190),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(435, height()-190),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(635, height()-295),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(635, height()-295),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(835, height()-400),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(835, height()-400),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(1035, height()-505),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(1030, height()-505),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
-add([
-    rect(100, 20),
-    pos(1125, height()-595),
-    area(),
-    body({isStatic: true}),
-    color(255, 255, 0)
-])
+    add([
+        rect(100, 20),
+        pos(1200, height()-595),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
 
 
-add([
-    circle(85),
-    pos(width()-200, 130),
-    color(255, 255, 255)
-])
+    const moon = add([
+        circle(85),
+        pos(width()-250, 130),
+        color(255, 255, 255),
+        area()
+    ])
 
-// add character to screen, from a list of components
-const player = add([
-    sprite("bean"),  // renders as a sprite
-    pos(120, 80),    // position in world
-    area(),          // has a collider
-    body()          // responds to physics and gravity
-])
+    // add character to screen, from a list of components
+    const player = add([
+        sprite("bean"),  // renders as a sprite
+        pos(120, 80),    // position in world
+        area(),          // has a collider
+        body(),          // responds to physics and gravity
+        "player"
+    ])
 
-// jump when player presses "space" key
-onKeyDown("space", () => {
-    if (player.isGrounded()){
-        player.jump()
-    }
+    // jump when player presses "space" key
+    onKeyDown("space", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("up", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("left", () => {
+        player.move(-speed, 0)
+    })
+
+    onKeyDown("right", () => {
+        player.move(speed, 0)
+    })
+
+    moon.onCollide("player", ()=>{
+        go("level_2")
+    })
+
+});
+
+go("level_1")
+
+scene("level_2", ()=>{
+    setGravity(1950)
+
+    // load a default sprite
+    loadBean()
+
+    const speed = 320
+
+    add([
+        rect(width(), 40),
+        pos(0, height()-40),
+        area(),
+        body({isStatic: true}),
+        color(0, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(250, height()-100),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(435, height()-190),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(635, height()-295),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(835, height()-400),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(1030, height()-505),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(1200, height()-595),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+
+    const moon = add([
+        circle(85),
+        pos(width()-250, 130),
+        color(0, 255, 255),
+        area(),
+    ])
+
+    // add character to screen, from a list of components
+    const player = add([
+        sprite("bean"),  // renders as a sprite
+        pos(120, 80),    // position in world
+        area(),          // has a collider
+        body(),
+        "player"          // responds to physics and gravity
+    ])
+
+    // jump when player presses "space" key
+    onKeyDown("space", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("up", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("left", () => {
+        player.move(-speed, 0)
+    })
+
+    onKeyDown("right", () => {
+        player.move(speed, 0)
+    })
+
+    moon.onCollide("player", ()=>{
+        go("level_3")
+    })
+
 })
 
-onKeyDown("up", () => {
-    if (player.isGrounded()){
-        player.jump()
-    }
+scene("level_3", ()=>{
+    setGravity(1950)
+
+    // load a default sprite
+    loadBean()
+
+    const speed = 320
+
+    add([
+        rect(width(), 40),
+        pos(0, height()-40),
+        area(),
+        body({isStatic: true}),
+        color(0, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(250, height()-100),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(435, height()-190),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(635, height()-295),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(835, height()-400),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(1030, height()-505),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+    add([
+        rect(100, 20),
+        pos(1200, height()-595),
+        area(),
+        body({isStatic: true}),
+        color(255, 255, 0)
+    ])
+
+
+    const moon = add([
+        circle(85),
+        pos(width()-250, 130),
+        color(0, 255, 255),
+        area()
+    ])
+
+    // add character to screen, from a list of components
+    const player = add([
+        sprite("bean"),  // renders as a sprite
+        pos(120, 80),    // position in world
+        area(),          // has a collider
+        body(),
+        "player"          // responds to physics and gravity
+    ])
+
+    // jump when player presses "space" key
+    onKeyDown("space", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("up", () => {
+        if (player.isGrounded()){
+            player.jump()
+        }
+    })
+
+    onKeyDown("left", () => {
+        player.move(-speed, 0)
+    })
+
+    onKeyDown("right", () => {
+        player.move(speed, 0)
+    })
+
+    //moon.onCollide("player", ()=>{
+        //go("level_4")
+   // })
 })
 
-onKeyDown("left", () => {
-    player.move(-speed, 0)
-})
-
-onKeyDown("right", () => {
-    player.move(speed, 0)
-})
